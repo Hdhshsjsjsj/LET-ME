07fb658de3616247786aaee3a636b7c3bfa9e91c
Hdhshsjsjsj <ilashadam039@gmail.com>
2024-07-10 00:06:51 +0400

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190351641'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CINEMAWORLD_123')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), True)
